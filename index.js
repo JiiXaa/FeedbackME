9e6e31f1a1784fec85829297e2011f1051ea3291
JiiXaa <anythingfx@gmail.com>
2021-12-07 14:12:29 +0000

    changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ updated: 'true' });
 });
 
 app.listen(PORT, () => {
