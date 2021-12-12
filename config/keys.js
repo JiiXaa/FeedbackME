6ee5cd7262c6c3648df48e0b5d500db7bd3c1381
JiiXaa <anythingfx@gmail.com>
2021-12-12 21:16:17 +0000

    added proxy: true in google strategy settings

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // keys.js - figure out what set of credentials to return
-// .NODE_ENV === 'production' is automatically created by HEROKU
+// .NODE_ENV === 'production' is automatically created by HEROKU you can see it after pushing to heroku NODE_ENV=production and other configs
 if (process.env.NODE_ENV === 'production') {
   // we are in production - return the production set of keys
   module.exports = require('./prod');
--- services/passport.js
+++ services/passport.js
@@ -23,6 +23,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({
